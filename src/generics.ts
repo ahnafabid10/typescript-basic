fa6db9ae696be676580e95fafe4d64858f314d71
Ahnaf Abid <ahnafabid600@gmail.com>
2026-05-08 17:42:21 +0600

    generics in ts

---
+++ src/generics.ts
@@ -0,0 +1,36 @@
+// dynamically generalize: Generic
+
+type GenericArray<value>  =Array<value>
+
+// const friends: string[] = ['x', 'y', 'z']
+const friends: GenericArray<string> = ['x', 'y', 'z']
+
+// const rollNumbers: number[] = [4,7,11]
+const rollNumbers: GenericArray<number> = [4,7,11]
+
+// const isEligibleList: boolean[] =[true , false, true]
+const isEligibleList: GenericArray<boolean> =[true , false, true]
+
+
+const userList: GenericArray<{name: string; age: number}> = [
+    {
+        name: 'Mr.x ',
+        age: 22
+    },
+    {
+        name: 'Mr. Y',
+        age: 21
+    },
+]
+
+
+const sqrFunc = (value: number)=>{
+    return value * value
+}
+sqrFunc(8)
+
+type Coordinates<X,Y> = [X,Y]
+
+const coordinates1: Coordinates<number, number> =[20,30]
+const coordinates2: Coordinates<string, string> =['20','30']
+
