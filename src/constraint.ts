ee35bdbc27ee9b5c644fed0f8981e8a791347a54
Ahnaf Abid <ahnafabid600@gmail.com>
2026-05-11 00:38:21 +0600

    Constraint in typescript

---
+++ src/constraint.ts
@@ -0,0 +1,33 @@
+// constrain: strict rules deya
+
+type Student = {id: number, name: string, dateOfBirth: string, class: string }
+
+const addStudentToCourse = <T extends Student>(studentInfo: T)=>{
+    return {
+        course: 'Next Level',
+        ...studentInfo
+    }
+}
+
+const student1 = {
+    id: 123,
+    name: 'Abis',
+    hasPen: true
+};
+const student2 = {
+    id: 523,
+    name: 'bids',
+    hasCar: true,
+    isMarried: true
+}
+
+const student3 = {
+    id:32,
+    name: 'Vibu',
+    handWatch: true,
+    dateOfBirth: "20-10-200",
+    class: "3"
+}
+
+const result = addStudentToCourse(student3);
+console.log(result)
